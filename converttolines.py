629c4fef1190b5d7cf03b8dba9527764db142cd6
Chintan Acharya <chintan.acharya27@gmail.com>
2021-10-04 11:52:18 +0530

    Converted to function

--- converttolines.py
+++ converttolines.py
@@ -1,84 +1,88 @@
 import cv2
 import numpy as np
 #import image
-image = cv2.imread('abcde.jpeg')
-image_1 = cv2.imread('abcde.jpeg')
-image_2 = cv2.imread('abcde.jpeg')
-# cv2.imshow('orig',image)
-# cv2.waitKey(0)
-
-# grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow('gray', gray)
-cv2.waitKey(0)
-cv2.imwrite("results/gray.png", gray)
-
-# binary
-ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
-cv2.imshow('second', thresh)
-cv2.waitKey(0)
-cv2.imwrite("results/thresh.png", thresh)
 
 
-# dilation
-kernel = np.ones((1, 100), np.uint8)
-# kernel = np.ones((100, 10), np.uint8)
-img_dilation = cv2.dilate(thresh, kernel, iterations=1)
-cv2.imshow('dilated', img_dilation)
-cv2.waitKey(0)
-cv2.imwrite("results/dilated.png", img_dilation)
+def convert_to_words(imagepath):
+    image = cv2.imread(imagepath)
+    image_1 = cv2.imread(imagepath)
+    image_2 = cv2.imread(imagepath)
+    # cv2.imshow('orig',image)
+    # cv2.waitKey(0)
 
+    # grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # cv2.imshow('gray', gray)
+    # cv2.waitKey(0)
+    cv2.imwrite("results/gray.png", gray)
 
-# find contours
-ctrs, hier = cv2.findContours(
-    img_dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # binary
+    ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+    # cv2.imshow('second', thresh)
+    # cv2.waitKey(0)
+    cv2.imwrite("results/thresh.png", thresh)
 
-# sort contours
-sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+    # dilation
+    kernel = np.ones((1, 100), np.uint8)
+    # kernel = np.ones((100, 10), np.uint8)
+    img_dilation = cv2.dilate(thresh, kernel, iterations=1)
+    # cv2.imshow('dilated', img_dilation)
+    # cv2.waitKey(0)
+    cv2.imwrite("results/dilated.png", img_dilation)
 
-for i, ctr in enumerate(sorted_ctrs):
-    # Get bounding box
-    x, y, w, h = cv2.boundingRect(ctr)
+    # find contours
+    ctrs, hier = cv2.findContours(
+        img_dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Getting ROI
-    roi = image[y:y+h, x:x+w]
+    # sort contours
+    sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+    count = 0
+    for i, ctr in enumerate(sorted_ctrs):
+        # Get bounding box
+        x, y, w, h = cv2.boundingRect(ctr)
 
-    kernel_word = np.ones((100, 10), np.uint8)
-    gray_word = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        # Getting ROI
+        roi = image[y:y+h, x:x+w]
 
-    ret_word, thresh_word = cv2.threshold(
-        gray_word, 127, 255, cv2.THRESH_BINARY_INV)
+        kernel_word = np.ones((100, 10), np.uint8)
+        gray_word = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
-    img_dilation_word = cv2.dilate(thresh_word, kernel_word, iterations=1)
+        ret_word, thresh_word = cv2.threshold(
+            gray_word, 127, 255, cv2.THRESH_BINARY_INV)
 
-    ctrs_word, hier_word = cv2.findContours(
-        img_dilation_word.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        img_dilation_word = cv2.dilate(thresh_word, kernel_word, iterations=1)
 
-    sorted_ctrs_words = sorted(
-        ctrs_word, key=lambda ctr: cv2.boundingRect(ctr)[0])
+        ctrs_word, hier_word = cv2.findContours(
+            img_dilation_word.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    for j, ctr_word in enumerate(sorted_ctrs_words):
-        x_word, y_word, w_word, h_word = cv2.boundingRect(ctr_word)
-        cv2.rectangle(roi, (x_word, y_word), (x_word +
-                      w_word, y_word + h_word), (84, 24, 62), 2)
-        cv2.rectangle(image_1, (x + x_word, y + y_word), (x + x_word +
-                      w_word, y + y_word + h_word), (84, 24, 62), 2)
+        sorted_ctrs_words = sorted(
+            ctrs_word, key=lambda ctr: cv2.boundingRect(ctr)[0])
 
-    # show ROI
-    cv2.imshow('segment no:'+str(i), roi)
-    cv2.rectangle(image, (x, y), (x + w, y + h), (90, 0, 255), 2)
-    cv2.rectangle(image_2, (x, y), (x + w, y + h), (90, 0, 255), 2)
-    cv2.waitKey(0)
+        for j, ctr_word in enumerate(sorted_ctrs_words):
+            x_word, y_word, w_word, h_word = cv2.boundingRect(ctr_word)
+            cv2.rectangle(roi, (x_word, y_word), (x_word +
+                                                  w_word, y_word + h_word), (84, 24, 62), 2)
+            cv2.rectangle(image_1, (x + x_word, y + y_word), (x + x_word +
+                                                              w_word, y + y_word + h_word), (84, 24, 62), 2)
+            word = image_1[y + y_word: y + y_word + h_word, x + x_word: x + x_word +
+                           w_word]
+            cv2.imwrite("words/" + str(count+1) + ".png", word)
+            count += 1
 
-cv2.imshow('marked areas', image_2)
-cv2.waitKey(0)
-cv2.imwrite("results/sentences.png", image_2)
+        # show ROI
+        # cv2.imshow('segment no:'+str(i), roi)
+        # cv2.rectangle(image, (x, y), (x + w, y + h), (90, 0, 255), 2)
+        # cv2.rectangle(image_2, (x, y), (x + w, y + h), (90, 0, 255), 2)
+        # cv2.waitKey(0)
 
-cv2.imshow('marked areas', image_1)
-cv2.waitKey(0)
-cv2.imwrite("results/words.png", image_1)
+    # cv2.imshow('marked areas', image_2)
+    # cv2.waitKey(0)
+    # cv2.imwrite("results/sentences.png", image_2)
 
+    # cv2.imshow('marked areas', image_1)
+    # cv2.waitKey(0)
+    # cv2.imwrite("results/words.png", image_1)
 
-cv2.imshow('marked areas', image)
-cv2.waitKey(0)
-cv2.imwrite("results/result.png", image)
+    # cv2.imshow('marked areas', image)
+    # cv2.waitKey(0)
+    # cv2.imwrite("results/result.png", image)
